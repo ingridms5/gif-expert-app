93f9487b3114cb0b3e408b0acb9603d7aef21e98
Ingrid Moncayo <ingridmoncayo5@gmail.com>
2022-05-27 15:48:58 -0500

    commit initial

---
+++ docs/precache-manifest.a058a2b09095a489dff4e7adaccbbaf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4372e8c8734b104efe0b46ffaeb8054f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d7b84f4e6e0e1dff3d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d7b84f4e6e0e1dff3d",
+    "url": "/static/js/main.04b20c7a.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
